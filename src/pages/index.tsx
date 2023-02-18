4d1e82d720b4805cbe603c29c5d756403be15ab7
Ruan Felipe Duarte Pontes <rufedupo@gmail.com>
2023-02-18 17:12:38 -0300

    Adição de icone de pagina

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <title>Codex Mutatio</title>
         <meta name="description" content="App inspirado no livro das mutações." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="https://diretoaoponto-tech.com.br/icon-target.png"/>
       </Head>
       <main className={styles.main}>
         <TSendTextOracle />
