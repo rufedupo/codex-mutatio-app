3edbbbc24a376485518350542959e8261a587870
Ruan Felipe Duarte Pontes <rufedupo@gmail.com>
2023-02-18 17:13:27 -0300

    Adição de icone ao index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <title>Codex Mutatio</title>
         <meta name="description" content="App inspirado no livro das mutações." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" type="image/png" href="https://diretoaoponto-tech.com.br/icon-target.png"/>
+        <link rel="icon" type="image/png" href="https://raw.githubusercontent.com/rufedupo/codex-mutatio-app/main/public/assets/images/iconPage/TartugaMutatioInvertido.png"/>
       </Head>
       <main className={styles.main}>
         <TSendTextOracle />
